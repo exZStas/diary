52a04f875dd0b4d6ad86c9d6b080034f6b1e6108
dmv <dmv@em70.ru>
2016-12-22 19:05:15 +0700

    Excluded schedule loading from parser

--- pp-backend/src/main/java/com/vm62/diary/backend/core/Parser.java
+++ pp-backend/src/main/java/com/vm62/diary/backend/core/Parser.java
@@ -16,50 +16,42 @@ import org.jsoup.select.Elements;
 public class Parser {
     private String groupName;
 
-    public Parser(String groupName) {
-        this.groupName = groupName;
+    public Parser() {
+
     }
 
-    public ArrayList<Event> parseSchedule() {
-        String scheduleURL = "http://rasp.tpu.ru/view.php?for=" + this.groupName + "&weekType=1";
+    public ArrayList<Event> parseSchedule(Document doc) {
         ArrayList<Event> scheduleEvents = new ArrayList<Event>();
-        Document doc;
 
-        try {
-            doc = Jsoup.connect(scheduleURL).get();
-            Elements scheduleTables = doc.select(".c-table.schedule");
-            Elements classesTimesElements = scheduleTables.select(".time");
-            ArrayList<String> classesTimes = new ArrayList<String>();
-            String bufTime;
+        Elements scheduleTables = doc.select(".c-table.schedule");
+        Elements classesTimesElements = scheduleTables.select(".time");
+        ArrayList<String> classesTimes = new ArrayList<String>();
+        String bufTime;
 
-            for (int i = 0; i < classesTimesElements.size(); i++) {
-                bufTime = classesTimesElements.get(i).text();
-                if (!classesTimes.contains(bufTime)) {
-                    classesTimes.add(bufTime);
-                }
+        for (int i = 0; i < classesTimesElements.size(); i++) {
+            bufTime = classesTimesElements.get(i).text();
+            if (!classesTimes.contains(bufTime)) {
+                classesTimes.add(bufTime);
             }
+        }
 
-            for (int i = 0; i < scheduleTables.size(); i++) {
-                Elements scheduleRows = scheduleTables.get(i).select("tr");
-                for (int j = 1; j < scheduleRows.size(); j++) {
-                    Elements scheduleColumns = scheduleRows.get(j).select("td");
-                    for (int k = 1; k < scheduleColumns.size(); k++) {
-                        Elements subject = scheduleColumns.get(k).select(".subject");
-                        if (subject.size() == 0) {
-                            continue;
-                        }
-                        String subjectDescription = scheduleColumns.get(k).select(".room a").text() + " " + scheduleColumns.get(k).select(".lesson-type").text() + " " + scheduleColumns.get(k).select(".group-teacher").text().trim();
-                        //TODO correct duration and date
-                        long userId = 1;
-                        long duration = 10000;
-                        Event classEvent = new Event(userId, subject.attr("title"), subjectDescription, Category.education, new Date(), new Date(), false, duration, "");
-                        scheduleEvents.add(classEvent);
+        for (int i = 0; i < scheduleTables.size(); i++) {
+            Elements scheduleRows = scheduleTables.get(i).select("tr");
+            for (int j = 1; j < scheduleRows.size(); j++) {
+                Elements scheduleColumns = scheduleRows.get(j).select("td");
+                for (int k = 1; k < scheduleColumns.size(); k++) {
+                    Elements subject = scheduleColumns.get(k).select(".subject");
+                    if (subject.size() == 0) {
+                        continue;
                     }
+                    String subjectDescription = scheduleColumns.get(k).select(".room a").text() + " " + scheduleColumns.get(k).select(".lesson-type").text() + " " + scheduleColumns.get(k).select(".group-teacher").text().trim();
+                    //TODO correct duration and date
+                    long userId = 1;
+                    long duration = 10000;
+                    Event classEvent = new Event(userId, subject.attr("title"), subjectDescription, Category.education, new Date(), new Date(), false, duration, "");
+                    scheduleEvents.add(classEvent);
                 }
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return scheduleEvents;
--- pp-backend/src/test/java/TestParser/TestParser.java
+++ pp-backend/src/test/java/TestParser/TestParser.java
@@ -2,9 +2,12 @@ package TestParser;
 
 import com.vm62.diary.backend.core.Parser;
 import com.vm62.diary.backend.core.entities.Event;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -13,8 +16,15 @@ import java.util.ArrayList;
 public class TestParser {
     @Test
     public void testParseHTML() {
-        Parser htmlParser = new Parser("8ВМ62");
-
-        ArrayList<Event> scheduleEvents = htmlParser.parseSchedule();
+        String scheduleURL = "http://rasp.tpu.ru/view.php?for=8ВМ62&weekType=1";
+        Document doc;
+        ArrayList<Event> scheduleEvents;
+        try {
+            doc = Jsoup.connect(scheduleURL).get();
+            Parser htmlParser = new Parser();
+            scheduleEvents = htmlParser.parseSchedule(doc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
